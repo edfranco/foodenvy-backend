e87546931a486aeb3743d6d3cdfb169160e2e343
edfranco <edfranco415@gmail.com>
2019-09-10 16:57:31 -0700

    added mongo_uri

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const DB_URL = 'mongodb://localhost:27017/food-envy';
+const DB_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/food-envy';
 
 mongoose.connect(DB_URL, {
     useNewUrlParser: true,
